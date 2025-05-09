e5fab2b59da4d586c7c878b8b410d87b184ef920
Pravanshu Samadhiya <pravanshusamadhiya685@gmail.com>
2025-05-09 23:12:35 +0530

    sjjs

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import RegisterPage from './pages/RegisterPage'
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import Dashboard from './pages/Dashboard'
 import PrivateRoute from './ components/PrivateRoute'
@@ -8,12 +8,15 @@ import DailySummary from './pages/DailySummary'
 
 const App = () => {
   return (
+    <BrowserRouter>
     <Routes>
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/" element={ <PrivateRoute>  <Dashboard/></PrivateRoute>} />
       <Route path="/summary" element={<PrivateRoute><DailySummary /></PrivateRoute>} />
     </Routes>
+    </BrowserRouter>
+    
   )
 }
 
