a1a7b4ea61b51d386f2631ca96d1f820f2ee2353
Pravanshu Samadhiya <pravanshusamadhiya685@gmail.com>
2025-05-09 22:10:18 +0530

    fix Cors Policy

--- Backend/index.js
+++ Backend/index.js
@@ -11,8 +11,18 @@ dotenv.config();
 
 const app = express();
 
+
+const allowedOrigins = [
+    'http://localhost:5173',
+  ];
+
+
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }));
+
 
 const PORT = process.env.PORT || 4000
 
